04c00eff9b686ce71fecd577732b8c4c28703e65
Hieu <nguyenconghieu@gmail.com>
2023-02-19 14:44:58 +0700

    Add project files.

---
+++ Singleton/KeyboardWindow.cs
@@ -0,0 +1,25 @@
+﻿internal class KeyboardWindow
+{
+    //field
+    private static KeyboardWindow instance;
+    public static KeyboardWindow Instance
+    {
+        get
+        {
+            if (instance == null)
+                instance = new KeyboardWindow();
+            return instance;
+        }
+    }
+    public double Height { get; set; }
+    public double Width { get; set; }
+
+    public override string ToString()
+    {
+        return $"Keyboard window has height{Height}, width{Width}";
+    }
+
+    private KeyboardWindow()
+    {
+    }
+}
---
+++ Singleton/Program.cs
@@ -0,0 +1,17 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+
+        KeyboardWindow window = KeyboardWindow.Instance;
+        window.Width = 10; window.Height = 5;
+        Console.WriteLine(window);
+        KeyboardWindow window1 = KeyboardWindow.Instance;
+        window1.Width = 15; window1.Height = 20;
+        Console.WriteLine(window1);
+        Console.WriteLine(window);
+
+
+        Console.ReadLine();
+    }
+}
